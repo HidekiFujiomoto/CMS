e6b023de8e4944b176db39640c06e8e8c983538e
HidekiFujimoto <kurosirobeagle@yahoo.co.jp>
2018-05-23 17:12:00 +0900

    メンター指摘箇所修正180523_C

--- app/mailers/cms_mailer.rb
+++ app/mailers/cms_mailer.rb
@@ -2,6 +2,6 @@ class CMSMailer < ApplicationMailer
   def send_email(user_email)
     @user_email = user_email
 
-    mail to: "kurosirobeagle@yahoo.co.jp", subject: "【CMS】新規メッセージを受信しました"
+    mail to: "kurosirobeagle@yahoo.co.jp", subject: "【CMS】新規お問い合わせ"
   end
 end
